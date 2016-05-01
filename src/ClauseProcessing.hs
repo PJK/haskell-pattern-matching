91cb98c361c6dd2748dca8be94f236e92a2b5761
PJK <me@pavelkalvoda.com>
2016-05-01 16:10:07 +0200

    Getting started

---
+++ src/ClauseProcessing.hs
@@ -0,0 +1,9 @@
+module ClauseProcessing where
+
+import Lib
+
+data ValueAbstractionSet = VAS [[Pattern]] deriving Show
+data ClauseCoverage = {  }
+
+patVecProc :: [Pattern] -> [[Pattern]] -> ([[Pattern]], )
+
