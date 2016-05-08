45cffaf9de0a5633a9c0986374e215aa7292555c
PJK <me@pavelkalvoda.com>
2016-05-08 18:02:43 +0200

    Partial test

---
+++ data/exact/PartialPatternWithVariable.hs
@@ -0,0 +1,15 @@
+module PartialPatternWithVariable where
+
+data MyTrinary = A | B | C
+
+data MyBool
+    = True
+    | False
+
+data MyMaybe = Sum MyTrinary | Nope
+
+
+partialExtract :: MyMaybe -> MyMaybe -> MyTrinary
+partialExtract (Sum C) (Sum A) = A
+partialExtract (Sum x)  y      = B
+partialExtract  Nope   (Sum C) = B
--- data/exact/PatternWithParameterVariableMatch.hs
+++ data/exact/PatternWithParameterVariableMatch.hs
@@ -12,3 +12,9 @@ extract :: MyMaybe -> MyMaybe -> MyTrinary
 extract (Sum C) (Sum A) = A
 extract (Sum x)  y      = B -- This should catch Sum a, Sum B, _
 extract  Nope    x      = B
+
+
+partialExtract :: MyMaybe -> MyMaybe -> MyTrinary
+partialExtract (Sum C) (Sum A) = A
+partialExtract (Sum x)  y      = B -- This should catch Sum a, Sum B, _
+partialExtract  Nope   (Sum C) = B
