67083234a252dc87239c53fab527b29cc2adf42f
PJK <me@pavelkalvoda.com>
2016-05-16 22:06:47 +0200

    Remove ununsed TypeMap

--- src/DataDefs.hs
+++ src/DataDefs.hs
@@ -88,7 +88,6 @@ guardType = "__anonymous_guard_type"
 
 
 type SimpleTypeMap = Map.Map String [Pattern]
-type TypeMap = Map.Map String [(Pattern, String)]
 
 -- |(Pattern, name of type)
 type TypedPattern = (Pattern, Type)
