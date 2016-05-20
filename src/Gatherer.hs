b02ecaaef442d92f8e1237aefa4287b826b40c85
PJK <me@pavelkalvoda.com>
2016-05-20 17:47:44 +0200

    Fix gamma construction

--- src/Gatherer.hs
+++ src/Gatherer.hs
@@ -80,6 +80,8 @@ initialGammas (Function _ functionType patterns)
         functionTypes = extractType functionType
 
         buildGamma :: [Type] -> [Pattern] -> Binding
+        buildGamma x y | trace ("Buildgamma: " ++ show (x, y)) False = error "asdfa"
+        buildGamma ts (GuardPattern _ _:ps) = buildGamma ts ps -- Do not bind guards
         buildGamma (t:ts) (VariablePattern name:ps)
             = Map.insert name t (buildGamma ts ps)
         buildGamma (_:ts) (_:ps) = buildGamma ts ps
